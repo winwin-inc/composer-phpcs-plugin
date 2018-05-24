c5645700e9ef84489f82b84bed321d5642c2f05b
Ties Verschuren <ties@lt-it.nl>
2018-05-24 14:16:03 +0200

    Fixed failed installation of pre-commit hook when the hooks directory is missing

--- src/PhpcsPlugin.php
+++ src/PhpcsPlugin.php
@@ -13,7 +13,7 @@ class PhpcsPlugin implements PluginInterface, EventSubscriberInterface
      * @var IOInterface
      */
     private $io;
-    
+
     public static function getSubscribedEvents()
     {
         return array(
@@ -29,10 +29,18 @@ class PhpcsPlugin implements PluginInterface, EventSubscriberInterface
 
     public function installGitPreHook()
     {
-        $hooksDir = '.git/hooks';
-        if (!is_dir($hooksDir)) {
+        $gitDir = '.git';
+        $hooksDir = $gitDir . '/hooks';
+        if (!is_dir($gitDir)) {
             $this->io->isVeryVerbose() && $this->io->writeError("No .git found, not in vcs?");
             return;
+        }
+        if (!is_dir($hooksDir)) {
+            $this->io->isVeryVerbose() && $this->io->write(sprintf("%s dir not found, create it..,", $hooksDir))
+            if (!mkdir($hooksDir)) {
+              $this->io->writeError(sprintf("Unable to create %s directory.", $hooksDir))
+              return;
+            }
         }
         if (file_exists($hookFile = $hooksDir . '/pre-commit')) {
             $this->io->isVeryVerbose() && $this->io->writeError("pre-commit hook file exists, skip");
