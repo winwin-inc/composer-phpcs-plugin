32d916765aa9de30455d3aa809924ac60efc67d8
Ye Wenbin <yewenbin@winwin-inc.com>
2019-03-22 18:34:13 +0800

    自动更新 hook 文件

--- src/PhpcsPlugin.php
+++ src/PhpcsPlugin.php
@@ -37,11 +37,13 @@ class PhpcsPlugin implements PluginInterface, EventSubscriberInterface
         if (!file_exists('.php_cs')) {
             copy(__DIR__.'/../resources/php_cs', '.php_cs');
         }
-        if (file_exists($hookFile = $hooksDir . '/pre-commit')) {
+        $newHookFile = __DIR__.'/../resources/pre-commit';
+        if (file_exists($hookFile = $hooksDir . '/pre-commit')
+            && md5_file($hookFile) == md5_file($newHookFile)) {
             $this->io->isVeryVerbose() && $this->io->writeError("pre-commit hook file exists, skip");
             return;
         }
-        copy(__DIR__.'/../resources/pre-commit', $hookFile);
+        copy($newHookFile, $hookFile);
         chmod($hookFile, 0755);
         $this->io->write("<info>Install git hook script $hookFile</>");
     }
