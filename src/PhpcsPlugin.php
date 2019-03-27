eff7a144e96ff66f234e0311d15604a878a086da
Ye Wenbin <yewenbin@winwin-inc.com>
2019-03-27 23:20:33 +0800

    fix pre-commit hook file may be remove by composer and signal error

--- src/PhpcsPlugin.php
+++ src/PhpcsPlugin.php
@@ -34,10 +34,14 @@ class PhpcsPlugin implements PluginInterface, EventSubscriberInterface
             $this->io->isVeryVerbose() && $this->io->writeError("No .git found, not in vcs?");
             return;
         }
+        $newHookFile = __DIR__.'/../resources/pre-commit';
+        if (!file_exists($newHookFile)) {
+            // maybe removed by composer
+            return;
+        }
         if (!file_exists('.php_cs')) {
             copy(__DIR__.'/../resources/php_cs', '.php_cs');
         }
-        $newHookFile = __DIR__.'/../resources/pre-commit';
         if (file_exists($hookFile = $hooksDir . '/pre-commit')
             && md5_file($hookFile) == md5_file($newHookFile)) {
             $this->io->isVeryVerbose() && $this->io->writeError("pre-commit hook file exists, skip");
